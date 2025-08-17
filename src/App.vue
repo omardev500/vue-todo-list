4bfa266d3afe01b12579cd67bf9b47f6007d4c9e
Omar Hossam <omarhossam50@proton.me>
2025-08-17 15:42:17 +0300

    Added Google Fonts, finished responsive & added CSS Vars

--- src/App.vue
+++ src/App.vue
@@ -106,26 +106,24 @@
 
 <template>
   <header class="relative pt-8">
-    <h1 class="text-center text-3xl font-bold uppercase">Todos Warrior</h1>
+    <h1 class="text-center font-(family-name:--font-1) text-lg sm:text-2xl md:text-3xl font-bold uppercase">Todos Warrior</h1>
     <button class="absolute top-5 right-4 bg-gray-200 text-orange-700 dark:text-blue-200 text-md dark:bg-gray-500 cursor-pointer p-2 rounded-lg" @click="toggleTheme"><i :class="themeIcon"></i></button>
   </header>
   <main class="pb-5">
-    <section class="relative w-screen mx-auto max-w-150 2xl:text-2xl xl:text-xl lg:text-lg lg:max-w-170 lg:min-h-130 xl:max-w-200 lg:min-h-145 mt-4 rounded-lg min-h-110 p-3 bg-slate-200 dark:bg-slate-700 pb-14">
+    <section class="relative font-(family-name:--font-1) text-sm sm:text-md max-w-[90%] w-screen mx-auto sm:max-w-150 2xl:text-2xl xl:text-xl lg:text-lg lg:max-w-170 lg:min-h-130 xl:max-w-200 lg:min-h-145 mt-4 rounded-lg min-h-110 p-3 bg-slate-200 dark:bg-slate-700 pb-14">
       <form @submit.prevent="addTodo" class="flex">
-        <input type="text" placeholder="Enter todo title.." v-model="todoTitle" required autofocus class="w-3/4  rounded-tl-md rounded-bl-md border-3 text-black dark:text-white border-r-0 border-purple-600 focus:border-purple-800 dark:border-slate-500 dark:focus:border-slate-400 outline-0 pl-2 bg-slate-150 placeholder:text-gray-600  dark:bg-slate-700 text-lg dark:placeholder:text-gray-300 transition duration-200" />
-        <button class="w-1/4 rounded-tr-md rounded-br-md p-3 bg-purple-600 hover:bg-purple-800 text-white font-bold dark:bg-slate-500 transition duration-300 dark:hover:bg-slate-400 cursor-pointer">Add Todo</button>
+        <input type="text" placeholder="Enter todo title.." v-model="todoTitle" required autofocus class="w-7/10 sm:w-3/4 rounded-tl-md rounded-bl-md border-3 text-black dark:text-white border-r-0 border-purple-600 focus:border-purple-800 dark:border-slate-500 dark:focus:border-slate-400 outline-0 pl-2 bg-slate-150 placeholder:text-gray-600  dark:bg-slate-700 text-md sm:text-lg dark:placeholder:text-gray-300 transition duration-200" />
+        <button class="w-3/10 sm:w-1/4 rounded-tr-md rounded-br-md p-2 sm:p-3 bg-purple-600 hover:bg-purple-800 text-white font-bold dark:bg-slate-500 transition duration-300 dark:hover:bg-slate-400 cursor-pointer">Add Todo</button>
       </form>
-      
-      <h2 class="font-mono underline text-lg pt-3 italic">Todos</h2>
-      <ul class="mt-2 mb-4 flex flex-col gap-1" v-if="ID > 0">
-        <li v-for="todo in filteredTodos" :key="todo.id" class="w-full p-[2px] pl-2 z-40 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-500 flex relative" :class="todo.done ? 'bg-gray-300 dark:bg-gray-500' : ''">
+
+      <ul class="my-4 flex flex-col gap-1" v-if="ID > 0">
+        <li v-for="todo in filteredTodos" :key="todo.id" class="w-full p-[2px] pl-2 z-40 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-500 sm:flex relative" :class="todo.done ? 'bg-gray-300 dark:bg-gray-500' : ''">
+          <input type="checkbox" class="accent-purple-600 inline-block dark:accent-slate-500 lg:text-xl" :checked="todo.done" :id="getTodoInputId(todo)" @click="todo.done = !todo.done"  />
+          <label :for="getTodoInputId(todo)" class="inline-block cursor-pointer lg:p-[6px] p-[5px] w-9/10 sm:w-7/10 pl-2 cursor-pointer" :class="todo.done && todoDone" >{{ todo.title }}</label>
           
-          <input type="checkbox" class="accent-purple-600 dark:accent-slate-500 lg:text-xl" :checked="todo.done" :id="getTodoInputId(todo)" @click="todo.done = !todo.done"  />
-          <label :for="getTodoInputId(todo)" class="cursor-pointer lg:p-[6px] p-[5px] w-7/10 pl-2 cursor-pointer" :class="todo.done && todoDone" >{{ todo.title }}</label>
-          
-          <div class="flex items-center justify-end w-3/10">
+          <div class="flex items-center sm:justify-end justify-end w-full sm:w-3/10">
   
-            <input v-bind="{value: todo.date || ''}" type="date" class="w-full h-full opacity-0 top-0 left-0 cursor-pointer dark:bg-gray-600 rounded-lg opacity-90" @change="todo.date = $event.target.value" />
+            <input v-bind="{value: todo.date || ''}" type="date" class="sm:w-full h-full opacity-0 top-0 left-0 cursor-pointer rounded-lg opacity-90" @change="todo.date = $event.target.value" />
             
             <button class="cursor-pointer rounded-sm z-41 hover:bg-red-400 dark:hover:bg-red-700 font-bold text-sm ml-3 p-1 mr-2" @click="removeTodo(todo)"><i class="fa-solid fa-x"></i></button>
           </div>
